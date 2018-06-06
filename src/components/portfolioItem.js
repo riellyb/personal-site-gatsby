fcc2d9269bfd787e8c5a80b54f220e816ef79a79
Brendan Rielly <brendanrielly@gmail.com>
2018-06-05 21:49:15 -0600

    added wrapper div for portfolio content

--- src/components/portfolioItem.js
+++ src/components/portfolioItem.js
@@ -3,10 +3,12 @@ import Link from 'gatsby-link'
 
 const PortfolioItem = (props) => (
   <div className={'main-work-gallery-item'}>
-    <a target="_blank" href={props.href}>
-    	<h3>{props.title}</h3>
-    	<img  style={{maxWidth: '100%', display: 'block'}} src={props.src} />
-    </a>
+  	<div className={'main-work-content'}>
+	    <a target="_blank" href={props.href}>
+	    	<h3>{props.title}</h3>
+	    	<img  style={{maxWidth: '100%', display: 'block'}} src={props.src} />
+	    </a>
+    </div>
   </div>
 )
 
