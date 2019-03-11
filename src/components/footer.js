8bf0036453a58c2aaf092df74c713c144a04faba
Brendan Rielly <brendan.rielly@endurance.com>
2019-03-11 19:07:09 -0400

    Upgraded to Gatsby V2

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,27 @@
-import React from 'react'
-import FooterItem from '../components/footerItem'
-import gitHubLogo from '../images/GitHub-Mark-64px.png'
-import linkedInLogo from '../images/In-Black-66px-TM.png'
-import instagramLogo from '../images/insta.png'
+import React from 'react';
+import FooterItem from '../components/footerItem';
+import gitHubLogo from '../images/GitHub-Mark-64px.png';
+import linkedInLogo from '../images/In-Black-66px-TM.png';
+import instagramLogo from '../images/insta.png';
 
 const Footer = () => (
-    <footer className='footer'>
-        <FooterItem image={gitHubLogo} href='https://github.com/riellyb' />
-        <FooterItem image={linkedInLogo} href='https://www.linkedin.com/in/brendan-rielly-5a30a821/' />
-        <FooterItem image={instagramLogo} href='https://www.instagram.com/brielly1421/' />
+    <footer className="footer">
+        <FooterItem
+            image={gitHubLogo}
+            href="https://github.com/riellyb"
+            alt={'Github'}
+        />
+        <FooterItem
+            image={linkedInLogo}
+            href="https://www.linkedin.com/in/brendan-rielly-5a30a821/"
+            alt={'LinkedIn'}
+        />
+        <FooterItem
+            image={instagramLogo}
+            href="https://www.instagram.com/brielly1421/"
+            alt={'Instagram'}
+        />
     </footer>
-)
+);
 
-export default Footer
+export default Footer;
--- src/components/footerItem.js
+++ src/components/footerItem.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-const FooterItem = (props) => (
-    <div className='footer-item'>
-    	<a target="_blank" href={props.href}><img src={props.image} /></a>
+const FooterItem = props => (
+    <div className="footer-item">
+        <a target="_blank" rel="noopener noreferrer" href={props.href}>
+            <img src={props.image} alt={props.alt} />
+        </a>
     </div>
-)
+);
 
-export default FooterItem
+export default FooterItem;
