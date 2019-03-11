9f2dfe731ef62a5478fbfbf64c0a7d559439dc94
Brendan Rielly <brendan.rielly@endurance.com>
2019-03-11 19:20:05 -0400

    Fixed gatsby link references

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby';
 
 const NotFoundPage = () => (
-  <div>
-    <h1>404<br />NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist.</p>
-    <Link to='/'>Back to the Homepage</Link>
-  </div>
-)
+    <div>
+        <h1>
+            404<br />NOT FOUND
+        </h1>
+        <p>You just hit a route that doesn&#39;t exist.</p>
+        <Link to="/">Back to the Homepage</Link>
+    </div>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby';
 
 const ThanksPage = () => (
-  <div className="thanks">
-    <h1>Thanks!</h1>
-    <p>Thanks for contacting me,<br />I'll get back to you as soon as I can.</p>
-    <Link to="/">Go back to the homepage</Link>
-  </div>
-)
+    <div className="thanks">
+        <h1>Thanks!</h1>
+        <p>
+            Thanks for contacting me,<br />I'll get back to you as soon as I
+            can.
+        </p>
+        <Link to="/">Go back to the homepage</Link>
+    </div>
+);
 
-export default ThanksPage
+export default ThanksPage;
