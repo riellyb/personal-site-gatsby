4eb5d20cec156abff368ac6970becaf23c693f7e
Brendan Rielly <brendan.rielly@endurance.com>
2020-04-20 20:05:56 -0400

    Remove fixed header and get rid of active menu item

--- src/components/header.js
+++ src/components/header.js
@@ -2,36 +2,35 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 const Header = ({ siteTitle }) => (
-    <header className="header">
-        <nav className="main-nav fixed">
-            <div className="sitename">
-                <Link
-                    to="/"
-                    style={{
-                        color: 'white',
-                        textDecoration: 'none'
-                    }}>
-                    {siteTitle}
-                </Link>
-            </div>
-            <ul>
-                <li className="main-nav-item">
-                    <a href="#about">About</a>
-                </li>
-                <li className="main-nav-item">
-                    <a href="#work">Work</a>
-                </li>
-                <li className="main-nav-item">
-                    <a href="#photos">Photos</a>
-                </li>
-                <li className="main-nav-item">
-                    <a href="#contact" className="nav-active">
-                        Contact
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    </header>
+  <header className="header">
+    <nav className="main-nav">
+      <div className="sitename">
+        <Link
+          to="/"
+          style={{
+            color: 'white',
+            textDecoration: 'none',
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </div>
+      <ul>
+        <li className="main-nav-item">
+          <a href="#about">About</a>
+        </li>
+        <li className="main-nav-item">
+          <a href="#work">Work</a>
+        </li>
+        <li className="main-nav-item">
+          <a href="#photos">Photos</a>
+        </li>
+        <li className="main-nav-item">
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 );
 
 export default Header;
