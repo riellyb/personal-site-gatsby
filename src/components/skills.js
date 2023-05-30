dd32bb173314a3e0273a8e3a1297b5216fd0de50
Brendan Rielly <brielly@mural.co>
2023-05-30 15:23:30 -0400

    Update skills

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,31 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const Skills = () => (
   <div className="skills">
-    <div className="skill-column">
-      <h3>Very Experienced With:</h3>
-      <ul className="skills-list">
-        <li>HTML5</li>
-        <li>CSS3</li>
-        <li>Vanilla Javascript</li>
-        <li>JQuery</li>
-        <li>Sass</li>
-        <li>NPM</li>
-      </ul>
+    <div className="skills-section">
+      <b>Languages:</b> JavaScript, TypeScript, PHP
     </div>
-    <div className="skill-column">
-      <h3>Proficient With:</h3>
-      <ul className="skills-list">
-        <li>ReactJS</li>
-        <li>GatsbyJS</li>
-        <li>NextJS</li>
-        <li>PHP</li>
-        <li>MySQL</li>
-        <li>MongoDB</li>
-        <li>NodeJS</li>
-        <li>ExpressJS</li>
-        <li>Webpack</li>
-      </ul>
+    <div className="skills-section">
+      <b>Frameworks/Libraries:</b> ReactJS, NextJS, NodeJS, ExpressJS, GatsbyJS
+    </div>
+
+    <div className="skills-section">
+      <b>Testing:</b> Jest, React Testing Library, CucumberJS, Mocha, Chai
+    </div>
+
+    <div className="skills-section">
+      <b>Web Technologies:</b> HTML5, CSS3, Sass
+    </div>
+
+    <div className="skills-section">
+      <b>Version Control:</b> Git
+    </div>
+
+    <div className="skills-section">
+      <b>Databases:</b> MongoDB, MySQL
+    </div>
+    <div className="skills-section">
+      <b>Build Tools:</b> NPM, Grunt, Gulp, Webpack
     </div>
   </div>
 );
